12b821ae5d0ec0010c48c3a74f803da72e4b0892
baucl <israelcll1994@gmail.com>
2021-03-14 17:16:53 -0300

    add navigations stack and icons

--- app/navigations/Navigation.js
+++ app/navigations/Navigation.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
+import { Icon } from "react-native-elements";
 import RestaurantsStack from "./RestaurantsStack";
 import FavoritesStack from "./FavoritesStack";
 import AccountStack from "./AccountStack";
@@ -10,10 +10,46 @@ import TopRestaurantsStack from "./TopRestaurantsStack";
 
 const Tab = createBottomTabNavigator();
 
+const screenOptions = (route, color) => {
+  let iconName;
+  switch (route.name) {
+    case "restaurants":
+      iconName = "compass-outline";
+      break;
+    case "favorites":
+      iconName = "heart-outline";
+      break;
+    case "topRestaurants":
+      iconName = "star-outline";
+      break;
+    case "search":
+      iconName = "magnify";
+      break;
+      case "account":
+      iconName = "home-outline";
+      break;
+    default:
+      break;
+  }
+
+  return (
+    <Icon type="material-community" name={iconName} size={22} color={color} />
+  );
+};
+
 const Navigation = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        initialRouteName="restaurants"
+        tabBarOptions={{
+          inactiveTintColor: "#646464",
+          activeTintColor: "#00a680",
+        }}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color }) => screenOptions(route, color),
+        })}
+      >
         <Tab.Screen
           name="restaurants"
           component={RestaurantsStack}
@@ -28,7 +64,7 @@ const Navigation = () => {
             title: "Favoritos",
           }}
         />
-         <Tab.Screen
+        <Tab.Screen
           name="topRestaurants"
           component={TopRestaurantsStack}
           options={{
@@ -53,4 +89,5 @@ const Navigation = () => {
     </NavigationContainer>
   );
 };
+
 export default Navigation;
