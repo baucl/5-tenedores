5734cf6c8daee94a5b36b6caa8618505d3c240d2
baucl <israelcll1994@gmail.com>
2021-03-14 20:01:28 -0300

    fix keys

--- app/utils/firebase.js
+++ app/utils/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase/app";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyD6-kuvXSgKlJsHWeHvLCl34-YnYNq5KPY",
-  authDomain: "tenedores-a4fb8.firebaseapp.com",
-  databaseURL: "https://tenedores-a4fb8.firebaseio.com",
-  projectId: "tenedores-a4fb8",
-  storageBucket: "tenedores-a4fb8.appspot.com",
-  messagingSenderId: "72760166064",
-  appId: "1:72760166064:web:8aae5d9e7036df6788dd75",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 export const firebaseApp = firebase.initializeApp(firebaseConfig);
